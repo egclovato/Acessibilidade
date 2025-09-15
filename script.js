deb5d25a04d343fbae8f14133fdab98ec65404b0
egclovato <eliaslovato@professor.educacao.sp.gov.br>
2025-09-15 19:04:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ document.addEventListener('DOMContentLoaded', function(){
     botaoDeAcessibilidade.addEventListener('click', function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao')
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
     })
 
     const aumentaFonteBotao = document.getElementById('Aumentar-Fonte');
     const diminuiFonteBotao = document.getElementById('Diminuir-Fonte');
+
+    const alternaContraste = document.getElementById('Alterna-Contraste');
 
     let tamanhoAtualFonte = 1;
 
@@ -21,4 +26,13 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
+
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+    })
 })
+
+ScrollReveal().reveal('#inicio', {delay: 500});
+ScrollReveal().reveal('#tropicalia', {delay: 500});
+ScrollReveal().reveal('#galeria', {delay: 500});
+ScrollReveal().reveal('#contato', {delay: 500});
